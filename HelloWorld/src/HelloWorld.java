fd93f3f651ddcefb01c775350eba2c4eea0ececb
Lukas Kunzemann <ga68kol@mytum.de>
2015-12-12 16:32:15 +0100

    now added to a greeting

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,16 +3,26 @@
  */
 
 /**
- * @author Tobias
+ * @author Lukas
  *
  */
+import java.io.*;
+
 public class HelloWorld {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello ASE2015 - how are you? :)");
+		InputStreamReader isr = new InputStreamReader(System.in);
+	    BufferedReader br = new BufferedReader(isr);
+	    System.out.print("Please enter your name: ");
+	    String eingabe = br.readLine();
+	
+		HelloUser User = new HelloUser(eingabe);
+		User.greetUser();
 	}
 
 }
